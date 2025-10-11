8407b536958de21fa4763daeeacd1861be62dc9c
faizaanjum7 <faizaanjumm07@gmail.com>
2025-10-11 16:53:34 +0530

    tasks rearrange using dragdrop

--- mypomodoro/src/Agenda.jsx
+++ mypomodoro/src/Agenda.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { DragDropContext, Droppable, Draggable } from "hello-pangea/dnd";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 import "./App.css";
 
 function Agenda() {
